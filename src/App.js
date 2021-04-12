2a91aa9915eec73f6494af39d6ea19124ebd861c
Vladimir <freshcode.yt@gmail.com>
2021-04-12 16:01:51 +0300

    Add simple redux counter example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-function App () {
-  return <div></div>
+import { connect } from 'react-redux';
+function App (props) {
+  const { count, dispatch } = props;
+  const decrement = () => {
+    const action = { type: 'DECREMENT' };
+    dispatch(action);
+  };
+  const increment = () => {
+    const action = { type: 'INCREMENT' };
+    dispatch(action);
+  };
+  return (
+    <div>
+      <h1>Current counter value: {count}</h1>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
 }
-
-export default App
+function mapStateToProps (state) {
+  /* Подписываемся на состояние */
+  return state;
+}
+/* const withState = connect(mapStateToProps);
+const componentWithState = withState(App);
+export default componentWithState; */
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 
+const initialState = {
+  count: 0,
+};
+function reducer (state = initialState, action) {
+  switch (action.type) {
+    case 'DECREMENT': {
+      return {
+        count: state.count - 1,
+      };
+    }
+    case 'INCREMENT': {
+      return {
+        count: state.count + 1,
+      };
+    }
+    default:
+      return state;
+  }
+}
 
+const store = createStore(reducer);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
+/* 
+
+  REDUX:
+    store - хранилище. js object
+    reducer - pure func; (state,action) => state
+    dispatch - function которая отправляет action в reducer
+
+  REACT-REDUX:
+    connect - HOC. subscribe component to store
+    mapStateToProps - func, что именно из store взять
+    mapDispatchToProps
+
+
+*/
